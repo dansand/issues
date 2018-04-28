9578215fa52dde214a0421ca9614c5ea1a50c816
Dan Sandiford <sonderfjord@gmail.com>
2018-04-28 15:01:40 +1000

    added slade bug

--- min_max_auxiallary/min_max_crash.py
+++ min_max_auxiallary/min_max_crash.py
@@ -26,21 +26,39 @@ xFn = fn.input()[0]
 yFn = fn.input()[1]
 
 
-# In[13]:
+# In[19]:
+
+
+# initialise a swarm
+swarm = uw.swarm.Swarm( mesh=mesh )
+
+
+
+
+# In[29]:
 
 
 tWalls = mesh.specialSets["MaxJ_VertexSet"]
 
+swarmCoords = mesh.data[tWalls.data]
+swarm.add_particles_with_coordinates(swarmCoords)
+#exploring a workaround
 
-# In[15]:
+
+if not tWalls.data.shape[0]:
+    dumCoord = np.column_stack((mesh.data[:,0].mean(), mesh.data[:,1].mean()))
+    swarm.add_particles_with_coordinates(np.array(dumCoord))
+
+
+# In[27]:
 
 
 _minMax = fn.view.min_max(xFn , fn_auxiliary=yFn)
 _minMax.reset()
-_minMax.evaluate(tWalls);
+_minMax.evaluate(swarm);
 
 
-# In[16]:
+# In[28]:
 
 
 print(_minMax.min_global())
